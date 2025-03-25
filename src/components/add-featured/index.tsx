85b74556a8f7a8044e4de43eb7f26ce3a7fb1d32
George Mushi <adamprosper99@gmail.com>
2025-03-25 18:49:09 +0300

    update brand page

--- src/components/add-featured/index.tsx
+++ src/components/add-featured/index.tsx
@@ -9,19 +9,19 @@ const AdSlotsFeatured = () => {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const { data, error } = useSwr<AdSlot[]>("/api/ads", fetcher);
 
-  if (error) return <div>Failed to load featured ad slots</div>;
+  if (error) return <div>Failed to load featured ads</div>;
 
   return (
     <section className="section section-products-featured">
       <div className="container">
         <header className="section-products-featured__header">
-          <h3>Featured Ad Slots for You</h3>
+          <h3>Featured Ads for You</h3>
           <Link href="/ads" className="btn btn--rounded btn--border">
-            View All Ad Slots
+            View All
           </Link>
         </header>
 
-        <AdSlotsCarousel adSlots={data || []} />
+        <AdSlotsCarousel adSlots={data?.slice(0,6) || []} />
       </div>
     </section>
   );
