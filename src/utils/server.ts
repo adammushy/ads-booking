ea6aaeb125032e07271aecff932c561febf346b0
George Mushi <adamprosper99@gmail.com>
2025-03-26 05:03:53 +0300

    updated add to cart

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -2,4 +2,5 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-ecommerce-front.vercel.app";
+  // : "https://next-ecommerce-front.vercel.app";
+  :"https://ads-booking.vercel.app/";
